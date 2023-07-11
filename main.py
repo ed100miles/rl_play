13def01b1cfc2599feca370ef0fff73b74988ad1
Ed Miles <edmiles@Eds-MacBook-Pro.local>
2023-07-11 22:39:43 +0100

    m1 mbp requiremnts.txt

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from stable_baselines3 import A2C
 
 env = gym.make('LunarLander-v2', render_mode='human')
 env.reset()
-model = A2C('MlpPolicy', env, verbose=1)
+model = A2C('MlpPolicy', env, verbose=1)  # type: ignore
 model.learn(total_timesteps=100000)
 
 episodes = 10
 
 for episode in range(episodes):
-    obs =env.reset()
+    obs = env.reset()
     terminated = False
 
     while not terminated:
         env.render()
-        observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+        observation, reward, terminated, truncated, info = env.step(
+            env.action_space.sample())
         # print(reward)
 
 env.close()
-
